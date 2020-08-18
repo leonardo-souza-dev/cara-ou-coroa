3098de22bc65a13eebbfe2c8be7aa27cab649e81
Leonardo souza <leonardotreze@gmail.com>
2020-08-18 19:56:58 -0300

    updating json

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express')
 const app = express()
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
 const port = process.env.PORT || 3000
 
+const options = {
+  definition: {
+    info: {
+      title: "cara ou coroa api",
+      description: "cara ou coroa api"
+    }
+  }
+}
+
+//const swaggerSpec = swaggerJsDoc(options)
+//app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+// Routes
+/**
+ * @swagger
+ * 
+ * /api:
+ *   get:
+ *     description: Obter cara ou coroa aleatoriamente
+ *     responses:
+ *       200:
+ *         description: Sucesso
+ */
 app.get('/api', (req, res) => {
     let resultado = between(0,2);
-    res.json(resultado == 1 ? 'cara' : 'coroa')
+    res.json(resultado == 1 ? { resultado: 'cara' } : { resultado: 'coroa' })
 })
 
 app.listen(port, () => {
